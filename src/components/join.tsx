a951abf0791f5afa8deba77d66fdbecb05d810a2
vipshehbaz <learnfromshehbaz08@gmail.com>
2025-07-23 21:09:34 -0700

    feat: db integrate in  join page

--- src/components/join.tsx
+++ src/components/join.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./common/button";
 import Link from "next/link";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
 import toast from "react-hot-toast";
 
@@ -10,6 +10,41 @@ function Join() {
   const [email, setEmail] = useState("");
   const [isChecked, setIsChecked] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("Loading...");
+  const [subTitle, setSubTitle] = useState("Loading...");
+  const [subTitlePara, setSubTitlePara] = useState("Loading...");
+  const [paraOne, setParaOne] = useState("Loading...");
+  const [paraTwo, setParaTwo] = useState("Loading...");
+  const [Question, setQuestion] = useState("Loading...");
+  const [linkText, setLinkText] = useState("Loading...");
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    const getSectionData = async () => {
+      try {
+        const docRef = doc(db, "joinPage", "joinPageContent");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setTitle(data.title || "");
+          setSubTitle(data.subTitle || "");
+          setSubTitlePara(data.subTitlePara || "");
+          setParaOne(data.paraOne || "");
+          setParaTwo(data.paraTwo || "");
+          setQuestion(data.Question || "");
+          setLinkText(data.linkText || "");
+          setImage(data.image || "");
+        } else {
+          toast.error("No such document!");
+        }
+      } catch (error) {
+        toast.error("Error fetching join page data");
+        console.error("Error fetching join page data:", error);
+      }
+    };
+    getSectionData();
+  }, []);
 
   const validateEmail = (email: string) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -47,17 +82,17 @@ function Join() {
 
   return (
     <div className="max-w-[600px] mx-auto bg-[var(--grey)] my-5 rounded-lg">
-      <div className="join-bg relative">
+      <div
+        className="join-bg relative "
+        style={{ backgroundImage: `url(${image})` }}
+      >
         <h2 className="p-5 text-[var(--background)] absolute bottom-0 left-0 z-10 text-[32px] leading-tight font-bold">
-          Join <br /> the Dia le hÉireann
+          {title}
         </h2>
       </div>
       <div className="px-5 mt-6">
-        <p className="font-bold text-lg">Change has begun, be part of it.</p>
-        <p className="mt-5 mb-10 text-[var(--grey-300)]">
-          Join hundreds of thousands of party members as we fix the foundations
-          and rebuild Ireland. Email address
-        </p>
+        <p className="font-bold text-lg">{subTitle}</p>
+        <p className="mt-5 mb-10 text-[var(--grey-300)]">{subTitlePara}</p>
         <div className="p-4 pb-10">
           <form onSubmit={handleSubmit} noValidate>
             <input
@@ -99,32 +134,15 @@ function Join() {
               {loading ? "Submitting..." : "Get started"}
             </button>
           </form>
-          <p className="mt-6 text-sm text-[var(--grey-300)]">
-            Read more about how we use your data in our{" "}
-            <Link href="" className="underline">
-              Privacy Notice.
-            </Link>{" "}
-            You can unsubscribe at any time.
-          </p>
-          <p className="mt-6 text-sm text-[var(--grey-300)]">
-            Payments securely processed by GoCardless. GoCardless Ltd (company
-            registration number 07495895) is authorised by the Financial Conduct
-            Authority under the Payment Services Regulations 2017, registration
-            number 597190, for the provision of payment services. GoCardless
-            uses personal data as described in their{" "}
-            <Link href="" className="underline">
-              Privacy Notice.
-            </Link>
-          </p>
+          <p className="mt-6 text-sm text-[var(--grey-300)]">{paraOne}</p>
+          <p className="mt-6 text-sm text-[var(--grey-300)]">{paraTwo}</p>
           <Link
             href="/what-is-membership"
             className="mt-8 px-4 py-5 flex flex-col justify-between gap-y-5 bg-[var(--background)] rounded-lg border-[1px] border-[var(--line)]"
           >
-            <p className="font-bold text-lg">
-              What is Dia le hÉireann membership?
-            </p>
+            <p className="font-bold text-lg">{Question}</p>
             <Button
-              content="Learn more"
+              content={linkText}
               className="!bg-[var(--backgorund)] !text-[var(--primary)] !p-0"
             />
           </Link>
