beb384f65c25689faf141e9d011066cc932dcf83
vipshehbaz <learnfromshehbaz08@gmail.com>
2025-08-18 20:40:58 +0500

    feat: update privacy page according to instructions

--- src/components/privacy.tsx
+++ src/components/privacy.tsx
@@ -4,50 +4,56 @@ import Link from "next/link";
 import React from "react";
 
 function Privacy() {
-  const Items = [
-    {
-      title: "Privacy notices",
-      para: "find a collection of the labour Party's privacy notices",
-      link: "/privacy/privacy-notes",
-    },
-    {
-      title: "Your rights",
-      para: "Find out about your privacy rights and submit any rights requests you may have",
-      link: "/privacy/data-subject-rights",
-    },
-    {
-      title: "Your data",
-      para: "Frequently asked questions about personal data",
-      link: "/privacy/frequently-asked-questions-about-personal-data",
-    },
-  ];
-
   return (
     <div>
       <div className="md:h-[240px] h-[100px] flex items-center justify-start bg-[var(--grey)]">
         <div className="wrapper">
           <h1 className="text-[calc(1.475rem_+_1.7vw)] text-[var(--primary)]">
-            Data protection
+            Privacy Policy
           </h1>
         </div>
       </div>
-      <div className="md:pt-12 pt-6 md:container px-4 md:px-0 flex justify-center items-center mx-auto flex-wrap md:flex-row flex-col">
-        {Items.map((item, index) => (
-          <div
-            key={index}
-            className="p-8 md:mx-3 my-4 border border-[var(--line)] rounded-[10px] md:min-w-[600px] min-w-full md:h-[270px] h-[230px] flex flex-col justify-between"
-          >
-            <h2 className="text-[calc(1.305rem_+_.55vw)]">{item.title}</h2>
-            <p className="md:max-w-[333px] text-lg mt-4">{item.para}</p>
-            <Link
-              href={item.link}
-              className="text-[var(--primary)] hover:text-[var(--btn-hover-bg)] text-[1.35rem] flex items-center gap-3"
-            >
-              More information{" "}
-              <FontAwesomeIcon icon={faArrowRight} width={18} />
-            </Link>
-          </div>
-        ))}
+      <div className="md:pt-12 pt-6 md:max-w-[1000px] px-4 md:px-0 mx-auto">
+        <p className="mb-4">Last Updated: August 9, 2025</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum
+          laboriosam, nisi molestiae quos animi harum facere ullam. Ab natus eum
+          corporis pariatur maxime eaque sed quo quisquam, provident alias? a
+          maxime ratione recusandae dolores iure alias? Accusamus animi unde
+          tempora perspiciatis voluptas. Dicta, iure id rem corporis
+          perspiciatis sequi nesciunt nisi eos error sed. Voluptas, quis? Ex
+          repellat ut dolore. Amet doloribus similique nam dignissimos quos ad,
+          fuga aliquid dolorum. Sequi, maiores labore dolores adipisci iure
+          vitae eaque numquam facere rerum. Vero sit beatae aliquam! Debitis?
+          Vero odit eveniet molestiae voluptatem. Provident earum molestias,
+          doloribus inventore iste delectus quidem? Quam consequatur magnam,
+          laboriosam rerum dolores sunt incidunt amet eius obcaecati deserunt
+          tenetur dolor. Dolorem, enim assumenda?
+        </p>
+        <p className="font-bold mt-5 mb-2">1. Who We Are</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus
+          impedit reiciendis, non fugit reprehenderit adipisci similique nisi
+          cupiditate molestias. Tempore suscipit veniam doloribus quidem
+          similique quas perspiciatis beatae debitis maiores. Laborum
+          reprehenderit quasi magnam dolores ab cupiditate porro voluptatibus,
+          sunt nemo corporis libero repudiandae at, fugiat veniam quibusdam.
+          Amet qui quasi velit ex possimus optio voluptate nobis esse nulla a
+        </p>
+        <ul className="ps-7 list-disc mt-5">
+          <li className="mb-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            natus quos harum vero culpa suscipit ipsa ab necessitatibus
+            quibusdam unde obcaecati autem dicta optio dolores, tempora possimus
+            modi error placeat!
+          </li>
+          <li className="mb-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            natus quos harum vero culpa suscipit ipsa ab necessitatibus
+            quibusdam unde obcaecati autem dicta optio dolores, tempora possimus
+            modi error placeat!
+          </li>
+        </ul>
       </div>
     </div>
   );
