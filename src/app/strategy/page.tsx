cacedb2a89daf4570cd1fdc853bd203017bbff92
vipshehbaz <learnfromshehbaz08@gmail.com>
2025-08-19 19:22:34 +0500

    fix: indent issue on vision page and strategy page

--- src/app/strategy/page.tsx
+++ src/app/strategy/page.tsx
@@ -67,36 +67,56 @@ function page() {
             className="w-full"
             src={img}
             alt="background image"
-            width={300}
-            height={300}
+            width={1000}
+            height={1000}
           />
         ) : (
           <div className="h-[50vh] bg-[var(--grey-300)] animate-pulse" />
         )}
       </div>
       <div className="md:p-14 p-8 flex flex-col gap-6">
-        <p className="max-w-[1000px] mx-auto">{paraOne}</p>
-        <p className="max-w-[1000px] mx-auto">{paraTwo}</p>
-        <p className="max-w-[1000px] mx-auto min-w-[1000px]">{paraThree}</p>
+        <p className="max-w-[1000px] mx-auto md:min-w-[1000px] min-w-full">
+          {paraOne}
+        </p>
+        <p className="max-w-[1000px] mx-auto md:min-w-[1000px] min-w-full">
+          {paraTwo}
+        </p>
+        <p className="max-w-[1000px] mx-auto md:min-w-[1000px] min-w-full">
+          {paraThree}
+        </p>
       </div>
       <div className="md:p-14 p-8 bg-[var(--primary)] text-white ">
-        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">{hTextOne}</p>
+        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">
+          {hTextOne}
+        </p>
       </div>
       <div className="md:p-14 p-8">
-        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">{hTextTwo}</p>
+        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">
+          {hTextTwo}
+        </p>
       </div>
       <div className="md:p-14 p-8 bg-[var(--primary)] text-white ">
-        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">{hTextThree}</p>
+        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">
+          {hTextThree}
+        </p>
       </div>
       <div className="md:p-14 p-8">
-        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">{hTextFour}</p>
+        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">
+          {hTextFour}
+        </p>
       </div>
       <div className="md:p-14 p-8 bg-[var(--primary)] text-white ">
-        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">{hTextFive}</p>
+        <p className="text-[calc(1.305rem_+_.55vw)] max-w-[1000px] mx-auto">
+          {hTextFive}
+        </p>
       </div>
       <div className="md:p-14 p-8 flex flex-col gap-6">
-        <p className="max-w-[1000px] mx-auto">{paraFour}</p>
-        <p className="max-w-[1000px] mx-auto">{paraFive}</p>
+        <p className="max-w-[1000px] mx-auto md:min-w-[1000px] min-w-full">
+          {paraFour}
+        </p>
+        <p className="max-w-[1000px] mx-auto md:min-w-[1000px] min-w-full">
+          {paraFive}
+        </p>
       </div>
       <PreFooter />
       <Footer />
