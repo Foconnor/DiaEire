7f7f6f19b6d7adb8db835991c93517f9bfeac53f
vipshehbaz <learnfromshehbaz08@gmail.com>
2025-08-30 21:49:33 +0500

    feat: make prefooter update through admin

--- src/components/pre-footer.tsx
+++ src/components/pre-footer.tsx
@@ -2,170 +2,140 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 
 function PreFooter() {
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [categories, setCategories] = useState<any[]>([]);
+  const [openIndex, setOpenIndex] = useState<{
+    cat: number;
+    sub?: number;
+  } | null>(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "preFooter"));
+        const data: any[] = [];
+        querySnapshot.forEach((docSnap) => {
+          data.push({ id: docSnap.id, ...docSnap.data() });
+        });
+        setCategories(data);
+      } catch (err) {
+        console.error("Error fetching categories:", err);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  const toggleCategory = (catIndex: number) => {
+    setOpenIndex(
+      openIndex?.cat === catIndex && openIndex?.sub === undefined
+        ? null
+        : { cat: catIndex }
+    );
+  };
+
+  const toggleSubCategory = (catIndex: number, subIndex: number) => {
+    setOpenIndex(
+      openIndex?.cat === catIndex && openIndex?.sub === subIndex
+        ? null
+        : { cat: catIndex, sub: subIndex }
+    );
+  };
+
   return (
     <div className="md:border-y-[1px] border-[var(--line)] py-[30px] mt-[70px]">
       <div className="grid md:grid-cols-6 grid-cols-1 max-w-[1200px] w-[95%] m-auto">
+        {/* Logo */}
         <div className="md:px-3 relative">
           <Image
             style={{ filter: "grayscale(100%)" }}
             className="absolute 2xl:-left-[27px] xl:-left-[20px] xl:-top-[10px] md:-top-[2px] -top-[70px] -left-[0px] md:w-fit md:h-fit w-[300px] h-[65px]"
             src="/images/logo.png"
-            alt=""
+            alt="logo"
             width={400}
             height={400}
           />
         </div>
-        <div className="md:px-3">
-          <div
-            className="flex flex-col border-y-[1px] border-[var(--line)] py-2 md:py-0 md:border-none mt-5  md:mt-0 cursor-pointer md:cursor-default"
-            onClick={() => setOpenIndex(openIndex === 0 ? null : 0)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">Campaign</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
-            </div>
-            <a href="#"
-              className={`links  md:block coming-soon ${
-                openIndex === 0 ? "block" : "hidden"
-              }`}
+
+        {/* Dynamic Categories */}
+        {categories.map((cat, catIndex) => (
+          <div className="md:px-3" key={cat.id}>
+            {/* Main category */}
+            <div
+              className="flex flex-col border-y-[1px] border-[var(--line)] py-2 md:py-0 md:border-none mt-5 md:mt-0 cursor-pointer md:cursor-default"
+              onClick={() => toggleCategory(catIndex)}
             >
-              Volunteer
-            </a>
-          </div>
-        </div>
-        <div className="md:px-3">
-          <div
-            className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default"
-            onClick={() => setOpenIndex(openIndex === 1 ? null : 1)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">People</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
+              <div className="flex items-center justify-between">
+                <p className="link-heading">{cat.categoryName}</p>
+                <FontAwesomeIcon
+                  className={`text-[var(--grey-300)] md:!hidden ${
+                    openIndex?.cat === catIndex && openIndex?.sub === undefined
+                      ? "rotate-180"
+                      : ""
+                  }`}
+                  icon={faChevronDown}
+                  size="sm"
+                />
+              </div>
+
+              {/* Main links */}
+              {cat.links?.map((link: any, i: number) => (
+                <a
+                  key={i}
+                  href={link.url}
+                  className={`links md:block ${
+                    openIndex?.cat === catIndex && openIndex?.sub === undefined
+                      ? "block"
+                      : "hidden"
+                  } ${link.url === "" ? "coming-soon" : ""}`}
+                >
+                  {link.name}
+                </a>
+              ))}
             </div>
-            <a
-              href="#"
-              className={`links  md:block coming-soon ${
-                openIndex === 1 ? "block" : "hidden"
-              }`}
-            >
-              Meet the team
-            </a>
+
+            {/* Subcategories */}
+            {cat.subCategories?.map((sub: any, subIndex: number) => (
+              <div
+                key={subIndex}
+                className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default md:mt-6 mt-0"
+                onClick={() => toggleSubCategory(catIndex, subIndex)}
+              >
+                <div className="flex items-center justify-between">
+                  <p className="link-heading">{sub.subCategoryName}</p>
+                  <FontAwesomeIcon
+                    className={`text-[var(--grey-300)] md:!hidden ${
+                      openIndex?.cat === catIndex && openIndex?.sub === subIndex
+                        ? "rotate-180"
+                        : ""
+                    }`}
+                    icon={faChevronDown}
+                    size="sm"
+                  />
+                </div>
+
+                {sub.links?.map((link: any, i: number) => (
+                  <a
+                    key={i}
+                    href={link.url}
+                    className={`links md:block ${
+                      link.url === "" ? "coming-soon" : ""
+                    } ${
+                      openIndex?.cat === catIndex && openIndex?.sub === subIndex
+                        ? "block"
+                        : "hidden"
+                    }`}
+                  >
+                    {link.name}
+                  </a>
+                ))}
+              </div>
+            ))}
           </div>
-          <div
-            className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default md:mt-6 mt-0"
-            onClick={() => setOpenIndex(openIndex === 2 ? null : 2)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">Updates</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
-            </div>
-            <a
-              href="#"
-              className={`links  md:block coming-soon ${
-                openIndex === 2 ? "block" : "hidden"
-              }`}
-            >
-              Latest from our campaign
-            </a>
-          </div>
-        </div>
-        <div className="md:px-3">
-          <div
-            className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default"
-            onClick={() => setOpenIndex(openIndex === 3 ? null : 3)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">About Us</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
-            </div>
-            <a
-              href="#"
-              className={`links  md:block coming-soon ${
-                openIndex === 3 ? "block" : "hidden"
-              }`}
-            >
-              How we work
-            </a>
-          </div>
-        </div>
-        <div className="md:px-3">
-          <div
-            className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default"
-            onClick={() => setOpenIndex(openIndex === 4 ? null : 4)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">Events</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
-            </div>
-            <a
-              href="#"
-              className={`links  md:block coming-soon ${
-                openIndex === 4 ? "block" : "hidden"
-              }`}
-            >
-              Events 2025
-            </a>
-          </div>
-        </div>
-        <div className="md:px-3">
-          <div
-            className="flex flex-col border-b-[1px] border-[var(--line)] py-2 md:py-0 md:border-none cursor-pointer md:cursor-default"
-            onClick={() => setOpenIndex(openIndex === 5 ? null : 5)}
-          >
-            <div className="flex items-center justify-between">
-              <p className="link-heading">Community Patriots</p>
-              <FontAwesomeIcon
-                className={`text-[var(--grey-300)] md:!hidden ${
-                  openIndex === 2 ? "rotate-180 " : ""
-                }`}
-                icon={faChevronDown}
-                size="sm"
-              />
-            </div>
-            <a
-              href="https://sinnenadaoine.ie/"
-              className={`links  md:block ${
-                openIndex === 5 ? "block" : "hidden"
-              }`}
-            >
-              Sinne Na Daoine
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
